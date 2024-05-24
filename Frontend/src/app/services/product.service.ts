f3581ee67f9b31bf9ff804c547ef04392e13af5e
TalaatEysa <talaateysa@outlook.com>
2024-05-25 02:25:55 +0300

    creating product service

---
+++ Frontend/src/app/services/product.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Product } from '../models/product';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  private baseUrl = 'http://localhost:8000/api/products/';
+
+  constructor(private http: HttpClient) { }
+
+  getAllProducts(): Observable<Product[]> {
+    return this.http.get<any>(this.baseUrl).pipe(
+      map(response => response.data as Product[])
+    );
+  }
+}
