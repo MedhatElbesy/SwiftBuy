e68b7424948c99f1c64ba42735f5b40d852d402b
mohamed awad <mohamed.awad.elgammal@gmail.com>
2024-05-27 23:06:34 +0200

    create add products commponent in admin dashboard

--- Frontend/src/app/admin/products/products.component.ts
+++ Frontend/src/app/admin/products/products.component.ts
@@ -3,6 +3,7 @@ import { ProductService } from '../../services/product.service';
 import { Product } from '../../models/product';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import {  Router } from '@angular/router';
 
 @Component({
   selector: 'app-products',
@@ -13,11 +14,18 @@ import { CommonModule } from '@angular/common';
 })
 export class ProductsComponent implements OnInit{
   products:Product[]=[];
-  constructor(private ProductService:ProductService){}
+  constructor(private ProductService:ProductService, private router:Router){}
 
   ngOnInit(): void {
       this.ProductService.getAllProducts().subscribe((data:any)=>{
         this.products=data;
   });
+}
+goToAdd(){
+  this.router.navigate(['/dashboard/product/add']);
+}
+
+toggleStatus(product: Product): void {
+  //still need logic
 }
 }
