ca54bb7517081fd185951d28e1991d83b6f58ad7
mohamed mostafa <muhammed.moustafa.ahmed@gmail.com>
2024-05-29 00:32:13 +0300

    change in the update function in UserController and make the update in front-end

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { ProductsComponent } from './products/products.component';
 import {AdminComponent} from './admin/admin.component'
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ProductsComponent,AdminComponent,HttpClientModule],
+  imports: [RouterOutlet,ProductsComponent,AdminComponent,HttpClientModule,ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
