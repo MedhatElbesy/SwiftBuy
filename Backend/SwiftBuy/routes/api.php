45d0cea5f307717ff73bef753acf4be0416ed52f
TalaatEysa <talaateysa@outlook.com>
2024-05-29 01:23:40 +0300

    fixing some issues

--- Backend/SwiftBuy/routes/api.php
+++ Backend/SwiftBuy/routes/api.php
@@ -23,15 +23,18 @@ use App\Http\Controllers\CartController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::resource('categories', CategoryController::class);
+Route::resource('products', ProductController::class);
+
 Route::middleware('auth:sanctum','api')->group(function () {
     Route::resource('carts',CartController::class);
-    Route::get('user/products',[ProductController::class, 'index']);
+    // Route::get('user/products',[ProductController::class, 'index']);
 });
 
 
 // Route::prefix('api')->middleware(['auth:sanctum','api','web'])->group(function () {
 Route::prefix('api')->middleware(['auth:sanctum','api','web'])->group(function () {
-    Route::resource('categories', CategoryController::class);
+    // Route::resource('categories', CategoryController::class);
     // Route::get('user/products',[ProductController::class, 'index']);
     // Route::resource('product_images', ProductImageController::class);
     // Route::get('/categories/{id}/products', [CategoryController::class, 'getProducts']);
@@ -39,7 +42,7 @@ Route::prefix('api')->middleware(['auth:sanctum','api','web'])->group(function (
     // Route::resource('order-items', OrderItemController::class);
     // Route::get('users/{user_id}/orders/{order_id}', [OrderController::class, 'getOrderForUser']);
     // Route::get('users/{user_id}/orders', [OrderController::class, 'getOrdersForUser']);
-    // Route::get('user', [ProductController::class, 'search']);
+    // Route::get('user', [ProductController::class, 'search']);0
 });
 
 Route::group(["prefix" => "admin/"],function(){
@@ -54,9 +57,9 @@ Route::group(["prefix" => "admin/"],function(){
 
     Route::group(["middleware" => "auth:admin-api"] , function(){
         // http://localhost:8000/api/admin/products must be login as admin and send token to featch
-        Route::resource('products', ProductController::class);
+        // Route::resource('products', ProductController::class);
         //http://localhost:8000/api/admin/categories must be login as admin and send token to featch
-        Route::resource('categories', CategoryController::class);
+        // Route::resource('categories', CategoryController::class);
         //http://localhost:8000/api/admin/orders  must be login as admin and send token to featch
         Route::resource('orders', OrderController::class);
 
