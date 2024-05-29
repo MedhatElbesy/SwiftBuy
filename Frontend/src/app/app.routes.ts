e4bbd0ec9715cd4eed9df7c8e48dc4fe9ff97285
Rehab5 <rehabkosbar@gmail.com>
2024-05-29 23:00:52 +0300

    homePage

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { UserHomeComponent } from '../app/components/user-home/user-home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', component: UserHomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'contact', component: ContactComponent },
+];
