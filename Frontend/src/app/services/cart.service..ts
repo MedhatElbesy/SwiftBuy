3c5ffff82600f4cb17f5f2a312396dda7585eef0
TalaatEysa <talaateysa@outlook.com>
2024-06-01 00:22:05 +0300

    fixing cart service

--- Frontend/src/app/services/cart.service..ts
+++ Frontend/src/app/services/cart.service..ts
@@ -9,15 +9,16 @@ export class CartService {
   private apiUrl = 'http://127.0.0.1:8000/api/user/carts';
 
   constructor(private http: HttpClient) { }
-  private getHeaders() {
-    const token = localStorage.getItem('token'); // Assuming the token is stored in localStorage
-    return new HttpHeaders({
-      'Authorization': `Bearer ${token}`
-    });
-  }
+  // private getHeaders() {
+  //   const token = localStorage.getItem('token'); // Assuming the token is stored in localStorage
+  //   return new HttpHeaders({
+  //     'Authorization': `Bearer ${token}`
+  //   });
+  // }
 
   addToCart(product: any): Observable<any> {
-    return this.http.post(this.apiUrl, product,{ headers: this.getHeaders() });
+    // return this.http.post(this.apiUrl, product,{ headers: this.getHeaders() });
+    return this.http.post(this.apiUrl, product);
   }
 
   delete(id: number): Observable<any> {
